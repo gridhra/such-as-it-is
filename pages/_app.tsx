5b4417e75959a61d1a9a62ffdb673fdc67169e83
aquila <aquila.minakami@gmail.com>
2024-06-26 02:06:58 +0900

    feat(project): init project & create profile page as home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react"
+import { chakraTheme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={chakraTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
