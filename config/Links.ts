065c6a7f2bb15670b356afa48a60e61d16ba68a2
aquila <aquila.minakamigmail.com>
2024-09-04 00:37:44 +0900

    feat(link): GitHubのusername変更に追従

--- config/Links.ts
+++ config/Links.ts
@@ -1,5 +1,5 @@
 // 外部リンク系。環境変数から動的に読むまではいかないので、ここにハードコーディングしている
-export const gitHubUrl = 'https://github.com/aquila-takayama'
+export const gitHubUrl = 'https://github.com/grdhra'
 export const twitterUrl = 'https://twitter.com/such_as_it_is'
 export const scrapboxUrl = 'https://scrapbox.io/aquila'
 export const discordUrl = 'https://discord.gg/ECtFgmme'
